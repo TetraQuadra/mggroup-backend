8e490886594953f047e593d0c6054dc9e1cad3ff
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2024-04-07 11:00:45 +0300

    refactor: deleting user

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -2,20 +2,15 @@ const createErrorMessage = require("../../helpers/createErrorMessage");
 const User = require("../../models/user");
 
 const deleteUser = async (req, res, next) => {
+  const { userId } = req.params;
   try {
-    const user = await User.findById(req.params.userId);
-
-    if (!user) {
-      throw createErrorMessage(404);
-    }
-
-    const response = await User.findByIdAndDelete(req.params.userId);
+    const response = await User.findByIdAndDelete(userId);
 
     if (!response) {
-      throw createErrorMessage(500);
+      throw createErrorMessage(404, "User not found");
     }
 
-    res.status(200).json("ok");
+    res.status(200).json({ message: "User deleted successfully", userId });
   } catch (error) {
     const { status = 500, message = "Internal server error" } = error;
     res.status(status).json({ status, message });
